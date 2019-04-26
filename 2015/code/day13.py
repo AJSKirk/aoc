984e4bdf7161bdc81d1f7c9835c26af293fb6e22
Andrew Kirk <ajskirk@gmail.com>
2019-04-26 14:22:29 +0100

    Day 13 Part A

---
+++ 2015/code/day13.py
@@ -0,0 +1,47 @@
+import sys
+import termcolor_util as tc
+import itertools
+from collections import defaultdict
+
+
+def load_input(fname):
+    prefs = defaultdict(dict)  # People -> Person[Neighbour]: Happiness
+    multiplier = {'gain': 1, 'lose': -1}
+    with open(fname, 'r') as f:
+        for line in f:
+            person, _, change, units, *_, neighbour = line.strip()[:-1].split(' ')  # Star-unpacking is a cool trick
+            prefs[person][neighbour] = multiplier[change] * int(units)
+    return prefs
+
+
+def score(arrangement, prefs):
+    out = 0
+    for i, person in enumerate(arrangement):
+        left_neighbour = arrangement[i-1]
+        right_neighbour = arrangement[(i+1) % len(arrangement)]
+        out += prefs[person][left_neighbour] + prefs[person][right_neighbour]
+
+    return out
+
+
+def part_a(prefs):
+    people = list(prefs.keys())
+    first = people.pop()  # Fixed reference to exploit without loss of generality
+    return max(score((first,) + tail_permute, prefs) for tail_permute in itertools.permutations(people))
+
+
+def part_b(puzzle_input):
+    pass
+
+
+def main(fname):
+    puzzle_input = load_input(fname)
+    print(f'The Answer to Part A is: {tc.green(part_a(puzzle_input))}')
+    print(f'The Answer to Part B is: {tc.green(part_b(puzzle_input))}')
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print('USAGE: python [script.py] [input.txt]')
+    else:
+        main(*sys.argv[1:])
