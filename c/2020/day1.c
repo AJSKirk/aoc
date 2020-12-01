8c95253f872246e04fbffe255d744a85873d5aea
Andrew Kirk <ajskirk@gmail.com>
2020-12-01 17:36:01 +0000

    The need for speed

--- c/2020/day1.c
+++ c/2020/day1.c
@@ -14,15 +14,55 @@ int get_file_length(FILE *fp) {
 	return len;
 }
 
+void file_to_arr(FILE* fp, int* arr) {
+	int i, n;
+
+	i = 0;
+	while (fscanf(fp, "%d/n", &n) > 0) {
+		arr[i] = n;
+		i++;
+	}
+	return;
+}
+
+int compar(const void* a, const void *b) {
+	const int a_val = *(int*) a, b_val = *(int*) b;
+	return (a_val > b_val) - (a_val < b_val);
+}
+
+int twosum(int* arr, int len, int target) {
+	// Expects sorted array
+	int lhs = 0, rhs = len - 1, sum;
+
+	while (lhs < rhs) {
+		sum = arr[lhs] + arr[rhs];
+		if (sum == target) return arr[lhs] * arr[rhs];
+		else if (sum < target) lhs++; // Too small, get bigger
+		else rhs--; // Too big, get smaller
+	}
+	return -1; // Fail code
+}
+
+int threesum(int* arr, int len, int target) {
+	int i, subres;
+
+	for (i=0; i<len; i++) {
+		subres = twosum(&arr[i+1], len - i - 1, target - arr[i]);
+		if (subres > 0) return subres * arr[i];
+	}
+	return -1;
+}
+
+
 int main(int argc, char* argv[]) {
 	FILE *fp;
 	int file_length;
 	int* expenses;
 	int next_expense;
-	int i; int j; int k; int n;
+	int i; int j; int k;
 
-	if (argc != 2) {
-		printf("Usage: day1 <input.txt>");
+	if (argc != 3) {
+		printf("Usage: day1 <input.txt> <target_sum>");
 	}
 
 	fp = fopen(argv[1], "r");
@@ -31,39 +71,18 @@ int main(int argc, char* argv[]) {
 		exit(EXIT_FAILURE);
 	}
 
-	file_length= get_file_length(fp);
+	file_length = get_file_length(fp);
 
-	expenses= (int*) malloc(file_length * sizeof(int));
+	expenses = (int*) malloc(file_length * sizeof(int));
 
 	rewind(fp);
-	i = 0;
-	while (fscanf(fp, "%d/n", &n) > 0) {
-		expenses[i] = n;
-		i++;
-	}
+	file_to_arr(fp, expenses);
 
-	for (i=0; i < file_length; i++) {
-		for (j=i+1; j < file_length; j++) {
-			if (expenses[i] + expenses[j] == 2020) {
-				printf("The product of the matching entries is: %i\n", expenses[i] * expenses[j]);
-				break;
-			}
-		}
-	} // Should really find a way to break out of this outer loop
+	qsort(expenses, file_length, sizeof(int), compar);
 
-	for (i=0; i < file_length; i++) {
-		for (j=i+1; j < file_length; j++) {
-			if (expenses[i] + expenses[j] >= 2020) {
-				continue; // All expenses positive so now impossible to reach 2020 with a third
-			}
-			for (k=j+1; k < file_length; k++) { // I miss itertools
-				if (expenses[i] + expenses[j] + expenses[k] == 2020) {
-					printf("The product of the matching entries is: %i\n", expenses[i] * expenses[j] * expenses[k]);
-					break;
-				}
-			}
-		}
-	}
+	printf("The product of the matching entries is: %i\n", twosum(expenses, file_length, atoi(argv[2])));
+	printf("The product of the matching entries is: %i\n", threesum(expenses, file_length, atoi(argv[2])));
+
 	free(expenses);
 	return 0;
 }
