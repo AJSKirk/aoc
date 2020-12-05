c739af08d7ccec58e8221387a11999271d5042be
Andrew Kirk <ajskirk@gmail.com>
2020-12-05 12:03:00 +0000

    Recompiled valgrind working, verified no leaks, tidying up

--- c/2020/day05.c
+++ c/2020/day05.c
@@ -11,7 +11,7 @@ int main(int argc, char* argv[]) {
 	char rowstring[8], colstring[4];
 	int row, col, id,  largest_id = 0;
 	int seats[NUM_SEATS] = {0};
-	int my_id, i;
+	int i;
 
 	while (scanf("%7s%3s\n", rowstring, colstring) > 0) {
 		row = find_element(rowstring, 7);
@@ -22,21 +22,21 @@ int main(int argc, char* argv[]) {
 	}
 
 	for (i=1; i<NUM_SEATS; i++) {
-		if (seats[i] == 0 && seats[i-1] == 1 && seats[i+1] == 1) break; // This could be faster
+		if (seats[i] == 0 && seats[i-1] == 1 && seats[i+1] == 1) break;
 	}
 
 	printf("The largest Ticket ID is: %d\n", largest_id);
-	printf("My Ticket ID is: %d\n", i);
+	printf("My Ticket ID is: %d\n", i); // Loop fallthrough, be careful
 }
 
 int find_element(char* pass,  int len) {
-	int lo = 0, hi, i, floor;
+	int lo = 0, hi, i, mid;
 	hi = (int) pow(2, len) - 1;
 
 	for (i=0; i<len; i++) {
-		floor = (hi + lo) / 2;
-		if (pass[i] == 'F' || pass[i] == 'L') hi = floor - 1;
-		else if (pass[i] == 'B' || pass[i] == 'R') lo  = floor + 1;
+		mid = (hi + lo) / 2;
+		if (pass[i] == 'F' || pass[i] == 'L') hi = mid - 1;
+		else if (pass[i] == 'B' || pass[i] == 'R') lo  = mid + 1;
 	}
 	return lo;
 }
