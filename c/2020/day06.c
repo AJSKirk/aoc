a4db02faa25cb7f4f99f5a8639698db9f89b3834
Andrew Kirk <ajskirk@gmail.com>
2020-12-06 12:02:44 +0000

    Bitmask set implementation of Day 6, still debugging part 2

--- c/2020/day06.c
+++ c/2020/day06.c
@@ -4,30 +4,34 @@
 #include <regex.h>
 #include <stdbool.h>
 
-int check_group(const char * group, int members);
+typedef unsigned int set_t; // 4 bytes is plenty
+
+set_t populate_set(const char *person);
+unsigned int get_cardinality(set_t set);
 
 int main(int argc, char* argv[]) {
-	char group[256], row[32];
+	char group[256], person[32];
 	int p_cursor = 0, len = 0;
-	char *next_row;
+	char *next_person;
 	int total = 0, members = 0, total_strict = 0;
+	set_t union_set = 0, intersection_set = 0, questions;
 
 	while (1) {
-		next_row = fgets(row, 32, stdin);
-		if (next_row == NULL || *row == '\n') {
-			p_cursor = 0;
-			total += check_group(group, 1);
-			total_strict += check_group(group, members);
-			members = 0;
+		next_person = fgets(person, 32, stdin);
+		if (next_person == NULL || *person == '\n') {
+			total += get_cardinality(union_set);
+			total_strict += get_cardinality(intersection_set);
+			printf("%d\n", get_cardinality(intersection_set));
+			union_set = 0;
+			intersection_set = ~0;
 		} else {
-			len = strnlen(row, 32);
-			row[len -  1] = ' '; // Hacky but it works to drop the newline
-			strncpy(&group[p_cursor], row, 255 - p_cursor); 
-			p_cursor += len;
-			members++;
+			person[strnlen(person, 32) - 1] = '\0'; // Drop the newline char
+			questions = populate_set(person);
+			union_set |= questions;
+			intersection_set &= questions;
 		}
 
-		if (next_row == NULL) break;
+		if (next_person == NULL) break;
 	}
 
 	printf("Total Count: %d\n", total);
@@ -35,28 +39,26 @@ int main(int argc, char* argv[]) {
 	return 0;
 }
 
-int check_group(const char * group, int members) {
-	// Set members=1 for Part 1
-	/* Hash tables would be more general, but 26 bytes is a small
-	 * price to pay for the speed here. Feel like it might bite me
-	 * in Part 2 though */
-	int letters[26] = {0};
-	char current;
-	int i = 0, total = 0, val_char;
-	int val_a = (int) 'a';
+set_t populate_set(const char *person) {
+	// Creates a bitmask-array implemented set for a person's answers
+	set_t questions = 0;
+	int i = 0, val_char;
+	const int val_a = (int) 'a';
 
-	while (group[i] != '\0') {
-		if (group[i] != ' ') { 
-			val_char = ((int) group[i]) - val_a;
-			letters[val_char]++;
-		}
+	while (person[i] != '\0') {
+		val_char = ((int) person[i]) - val_a;
+		questions |= (1U << val_char);  // Sets val_charth bit of questions to 1
 		i++;
 	}
 
-	for (i=0; i<26; i++) {
-		total += (letters[i] >= members);
-	}
-
-	return total;
+	return questions;
 }
 
+unsigned int get_cardinality(set_t set) {
+	int i, card = 0;
+
+	for (i=0; i<8*sizeof(set_t); i++) { // Remember sizeof is bytes, not bits
+		card += 1U & (set >> i);
+	}
+	return card;
+}
