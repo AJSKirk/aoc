ab4395291d28f5f949067202ba8857b8cfb22313
Andrew Kirk <ajskirk@gmail.com>
2020-12-07 12:59:48 +0000

    Day 7 part 2 - Breaks Part 1, not sure why yet

--- c/2020/day07.c
+++ c/2020/day07.c
@@ -8,37 +8,47 @@
 #define MAX_BAGS 1024
 #define BAG_BUFFER 32
 #define MAX_PARENTS 128
+#define MAX_CHILDREN 8
 
-struct child_bag {
+typedef struct {
 	int num;
 	char *name;
-};
+} child_bag;
 
 int count_ancestors(hash_t *bags, char *target, char **seen);
 bool check_seen(char *candidate, char **seen);
+int count_children(hash_t *bags, char *target);
 
 int main(int argc, char* argv[]) {
 	char row[256], *next_row;
 	char *parent_name, *next_child_name, *word;
 	char *target_container;
-	struct child_bag next_child;
+	child_bag *next_child;
+	int child_cursor = 0;
 
-	hash_t *bags = hash_new(MAX_BAGS);
+	hash_t *child_idx_bags = hash_new(MAX_BAGS);
+	hash_t *parent_idx_bags = hash_new(MAX_BAGS);
 
 	do {
 		next_row = fgets(row, 256, stdin);
+		// Don't free these calloced vars, the associative array needs that memory
 		parent_name = (char *) calloc(BAG_BUFFER, sizeof(char));
 		next_child_name = (char *) calloc(BAG_BUFFER, sizeof(char));
 		target_container = parent_name;
+		next_child = (child_bag *) calloc(1, sizeof(child_bag));
+		next_child->num = 1;
 		for (word=strtok(row, " ,.\n"); word; word=strtok(NULL, " ,.\n")) {
 			if ((strncmp(word, "contain", 7) == 0) || (strncmp(word, "bag", 3) == 0)) {
 				if (strnlen(next_child_name, 3) >= 3) {
-					hash_insert(bags, next_child_name, parent_name, MAX_PARENTS);
+					hash_insert(child_idx_bags, next_child_name, parent_name, MAX_PARENTS);
+					next_child->name = next_child_name;
+					hash_insert(parent_idx_bags, parent_name, next_child, MAX_CHILDREN);
+					next_child = (child_bag *) calloc(1, sizeof(child_bag));
 				}
 				next_child_name = (char *) calloc(BAG_BUFFER, sizeof(char));
 				target_container = next_child_name;
 			} else if (isdigit(*word)) {
-				next_child.num = atoi(word);
+				next_child->num = atoi(word);
 			}
 			else {
 				strncat(target_container, word, 32);
@@ -47,9 +57,9 @@ int main(int argc, char* argv[]) {
 	} while (next_row != NULL);
 
 	// Print array
-	int i = 0;
 	char **seen = (char **) calloc(MAX_BAGS, sizeof(char *));
-	printf("%d\n", count_ancestors(bags, TARGET_BAG, seen));
+	printf("%d\n", count_ancestors(child_idx_bags, TARGET_BAG, seen));
+	//printf("%d\n", count_children(parent_idx_bags, TARGET_BAG));
 }
 
 int count_ancestors(hash_t *bags, char *target, char **seen) {
@@ -64,7 +74,6 @@ int count_ancestors(hash_t *bags, char *target, char **seen) {
 		}
 		i++;
 	}
-	//free(parents); // We can free memory as we go
 	return count;
 }
 
@@ -78,4 +87,18 @@ bool check_seen(char *candidate, char **seen) {
 	}
 	seen[i] = candidate;
 	return true;
+}
+
+int count_children(hash_t *bags, char *target) {
+	int count = 0, i;
+	child_bag **children;
+	if (*target == '\0' || strncmp(target, "noother", BAG_BUFFER) == 0) {
+		return -1;
+	}
+	children = hash_lookup(bags, target);
+	for(i=0; children[i] != '\0'; i++) {
+		int res = children[i]->num * (1 + count_children(bags, children[i]->name));
+		count += res;
+	}
+	return count;
 }
