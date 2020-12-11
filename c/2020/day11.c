1bd29efa97b08e5a863216c3397dc80e1ef09c6f
Andrew Kirk <ajskirk@gmail.com>
2020-12-11 09:11:05 +0000

    Still ugly, still fast. Should OpenMP this one

--- c/2020/day11.c
+++ c/2020/day11.c
@@ -8,9 +8,11 @@
 
 //enum states {EMPTY: 0, OCCUPIED: 1, NOSEAT: 0};
 
-int step(char seats[][MAX_WIDTH], int height, int width);
+int step(char seats[][MAX_WIDTH], int height, int width, int thresh,
+		int (*neighbour_func)(char [][MAX_WIDTH], int, int, int, int));
 int count_occupied(char seats[][MAX_WIDTH], int height, int width);
-int count_neighbours(char seats[][MAX_WIDTH], int height, int width, int row, int col);
+int count_adjacent(char seats[][MAX_WIDTH], int height, int width, int row, int col);
+int count_visible(char seats[][MAX_WIDTH], int height, int width, int row, int col);
 
 int main(int argc, char* argv[]) {
 	int height=0, width=0, col=0;
@@ -28,29 +30,27 @@ int main(int argc, char* argv[]) {
 		}
 	}
 	
-
 	do {
 		;
-	} while (step(seats, height, width) != 0);
-
+	} while (step(seats, height, width, 5, &count_visible) != 0);
 
 	printf("%d\n", count_occupied(seats, height, width));
 
 	return 0;
 }
 
-int step(char seats[][MAX_WIDTH], int height, int width) {
+int step(char seats[][MAX_WIDTH], int height, int width, int thresh, 
+		int (*neighbour_func)(char [][MAX_WIDTH], int, int, int, int)) {
 	int row, col, delta = 0, neighbours;
 	int next[height][width];
 	int nrow, ncol;
 
 	for (row=0; row<height; row++) {
 		for (col=0; col<width; col++) {
-			neighbours = count_neighbours(seats, height, width, row, col);
-			if (seats[row][col] == 'L' && neighbours == 0) {
+			if (seats[row][col] == 'L' && neighbour_func(seats, height, width, row, col) == 0) {
 				next[row][col] = '#';
 				delta++;
-			} else if (seats[row][col] == '#' && neighbours >= 4) {
+			} else if (seats[row][col] == '#' && neighbour_func(seats, height, width, row, col) >= thresh) {
 				next[row][col] = 'L';
 				delta++;
 			} else {
@@ -64,6 +64,7 @@ int step(char seats[][MAX_WIDTH], int height, int width) {
 			seats[row][col] = next[row][col];
 		}
 	}
+
 	return delta;
 }
 
@@ -79,7 +80,7 @@ int count_occupied(char seats[][MAX_WIDTH], int height, int width) {
 	return count;
 }
 
-int count_neighbours(char seats[][MAX_WIDTH], int height, int width, int row, int col) {
+int count_adjacent(char seats[][MAX_WIDTH], int height, int width, int row, int col) {
 	int neighbours = 0;
 	int nrow, ncol;
 
@@ -92,4 +93,31 @@ int count_neighbours(char seats[][MAX_WIDTH], int height, int width, int row, in
 	}
 
 	return neighbours;
+}
+
+int count_visible(char seats[][MAX_WIDTH], int height, int width, int row, int col) {
+	int row_dir, col_dir;
+	int visible = 0;
+	int nrow, ncol;
+
+	for (row_dir=-1; row_dir <= 1; row_dir++) { // -1, 0, 1
+		for (col_dir=-1; col_dir <= 1; col_dir++) {
+			if (row_dir == 0 && col_dir == 0)
+				continue;
+			nrow = row;
+			ncol = col;
+			while (nrow >= 0 && nrow < height && ncol >= 0 && ncol < width) {
+				if (nrow == row && ncol == col)
+					;
+				else if (seats[nrow][ncol] == '#') {
+					visible += 1;
+					break;
+				} else if (seats[nrow][ncol] == 'L')
+					break;
+				nrow += row_dir;
+				ncol += col_dir;
+			}
+		}
+	}
+	return visible;
 }
