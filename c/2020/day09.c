d641c6089bd70d50c8ac7f006ca860654b67977c
Andrew Kirk <ajskirk@gmail.com>
2020-12-11 17:33:23 +0000

    Use stdlib bsearch() in Day 9

--- c/2020/day09.c
+++ c/2020/day09.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
+#include <sys/time.h>
 
 #define PRECURSORS_LEN 25
 #define SEEN_LEN 1024
@@ -16,6 +17,9 @@ int main(int argc, char* argv[]) {
 	long next, first_invalid = 0, expired = 0; // Too large for int!
 	long precursors_seen[PRECURSORS_LEN] = {0}, precursors_sort[PRECURSORS_LEN] = {0};
 	long seen[SEEN_LEN] = {0};
+	
+	struct timeval start, start_2, end;
+	gettimeofday(&start, NULL);
 	
 	while (scanf("%ld\n", &next) > 0) {
 		if (i >= PRECURSORS_LEN) {  // Skipped for preamble
@@ -36,28 +40,25 @@ int main(int argc, char* argv[]) {
 	}
 
 	printf("First Invalid Number: %ld\n", next);
+	gettimeofday(&start_2, NULL);
 	printf("Weakness: %ld\n", find_region(seen, i, next));
+	gettimeofday(&end, NULL);
+
+	printf("\nProblem 1 Time, including Parse: %d microseconds\n", start_2.tv_usec - start.tv_usec);
+	printf("Problem 2 Time: %d microseconds\n", end.tv_usec - start_2.tv_usec);
 	return 0;
 }
 
 void replace_precursor(long expired, long new, long sorted[]) {
 	// We know the list is sorted so we can binary search
-	int lo, hi, mid;
-	lo = 0;
-	hi = PRECURSORS_LEN - 1;
+	int *mid = NULL;
 
-	while (lo <= hi) {
-		mid = (lo + hi) / 2;
-		if (sorted[mid] < expired) {
-			lo = mid + 1;
-		} else if (sorted[mid] > expired) {
-			hi = mid - 1;
-		} else {
-			sorted[mid] = new;
-			return;
-		}
+	mid = bsearch(&expired, sorted, PRECURSORS_LEN, sizeof(long), compar);
+	if (mid == NULL) {
+		printf("Search for element %ld failed\n", expired);
+		exit(EXIT_FAILURE);
 	}
-	printf("Search for element %ld failed\n", expired);
+	*mid = new;
 }
 
 int compar(const void *a, const void *b) {
