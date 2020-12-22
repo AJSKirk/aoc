042b9ece7e30b251ad5671116ef97410473a3c5e
Andrew Kirk <ajskirk@gmail.com>
2020-12-22 14:33:28 +0000

    Better game encapsulation will carry over to recursive game

--- c/2020/day22.c
+++ c/2020/day22.c
@@ -5,15 +5,15 @@
 
 #define NUM_PLAYERS 2
 
-int determine_winner(int *cards);
+int play_combat_round(struct list_meta **decks);
 int smallest_deck(struct list_meta **decks);
 
 int main(int argc, char *argv[]) {
 	struct list_meta *decks[NUM_PLAYERS];
-	int card, i, cards[NUM_PLAYERS], winner, loser;
+	int card, i, winner;
 	char row[32];
 	int *winner_cards;
-	long score = 0;
+	int score = 0;
 
 	for (i=0; i<NUM_PLAYERS; i++) {
 		decks[i] = list_create();
@@ -24,33 +24,36 @@ int main(int argc, char *argv[]) {
 	}
 
 	while (smallest_deck(decks) > 0) {
-		for (i=0; i<NUM_PLAYERS; i++)
-			cards[i] = pop_head(decks[i]);
-
-		winner = determine_winner(cards);
-		loser = (winner == 1) ? 0 : 1;  // Only works in 2-player
-		push_tail(decks[winner], cards[winner]);
-		push_tail(decks[winner], cards[loser]);
+		winner = play_combat_round(decks);
 	}
 
+	// Winner of the game must be the winner of the last round
 	winner_cards = to_array(decks[winner]);
 	for (i=0; i<decks[winner]->length; i++)
 		score += winner_cards[i] * (decks[winner]->length - i);
 
-	printf("%ld\n", score);
+	printf("%d\n", score);
 
 	return 0;
 }
 
-int determine_winner(int *cards) {
-	int i, winner, max = INT_MIN;
+int play_combat_round(struct list_meta **decks) {
+	int cards[NUM_PLAYERS];
+	int i, winner, loser, max = INT_MIN;
 
 	for (i=0; i<NUM_PLAYERS; i++) {
+		cards[i] = pop_head(decks[i]);
 		if (cards[i] > max) {
 			max = cards[i];
 			winner = i;
 		}
 	}
+
+	loser = (winner == 1) ? 0 : 1;  // Only works in 2-player
+
+	push_tail(decks[winner], cards[winner]);
+	push_tail(decks[winner], cards[loser]);
+
 	return winner;
 }
 
@@ -63,3 +66,5 @@ int smallest_deck(struct list_meta **decks) {
 
 	return min;
 }
+
+//int play_recursive_combat(struuct list_meta
