32bb025ddd0e1f50104749660f48faad0f73424a
Andrew Kirk <ajskirk@gmail.com>
2021-02-10 12:11:59 +0000

    Making safe in the case of thread delays

--- python/y_2015/day04.py
+++ python/y_2015/day04.py
@@ -4,32 +4,41 @@ from hashlib import md5
 import multiprocessing
 
 
+CHECK_FREQ = 10000
+
+
 def compute_hash(secret_key: str, addendum: int) -> str:
     guess = secret_key + str(addendum)
     return md5(guess.encode()).hexdigest()
 
 
-def crack_worker(secret_key, leading_zeroes, start, step, result):
+def crack_worker(secret_key, leading_zeroes, start, step, smallest):
+    """Terminates once finding a working hash, or if it passes the current winner
+    Checks current winner every CHECK_FREQ iterations to reduce lock delays"""
     target = '0' * leading_zeroes
-    result.put(next(addendum for addendum in itertools.count(start, step)
-                    if compute_hash(secret_key, addendum).startswith(target)))
+    for i, addendum in enumerate(itertools.count(start, step)):
+        if compute_hash(secret_key, addendum).startswith(target):
+            if smallest.value == 0 or addendum < smallest.value:
+                smallest.value = addendum
+            return
+        if i % CHECK_FREQ == 0 and smallest.value != 0 and addendum >= smallest.value:
+            return
 
 
 def crack_hash(secret_key, leading_zeroes, start=0) -> int:
-    result = multiprocessing.Queue()
+    smallest = multiprocessing.Value('i', 0)
     num_cores = multiprocessing.cpu_count()
     threads = [multiprocessing.Process(target=crack_worker,
-                                       args=(secret_key, leading_zeroes, start + step, num_cores, result))
-               for step in range(num_cores)]
+                                       args=(secret_key, leading_zeroes, start + increment, num_cores, smallest))
+               for increment in range(num_cores)]
 
     for thread in threads:
         thread.start()
 
-    out = result.get()
-    for thread in threads:
-        thread.terminate()
+    for thread in threads:  # Wait for all threads to finish
+        thread.join()
 
-    return out
+    return smallest.value
 
 
 def main():
