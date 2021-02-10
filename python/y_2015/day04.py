418f75901772b1213423829163db4bfd812d771b
Andrew Kirk <ajskirk@gmail.com>
2021-02-10 15:14:15 +0000

    Leverage math.inf for cleaner min start

--- python/y_2015/day04.py
+++ python/y_2015/day04.py
@@ -2,6 +2,7 @@ import sys
 import itertools
 from hashlib import md5
 import multiprocessing
+import math
 
 
 CHECK_FREQ = 10000
@@ -12,21 +13,20 @@ def compute_hash(secret_key: str, addendum: int) -> str:
     return md5(guess.encode()).hexdigest()
 
 
-def crack_worker(secret_key, leading_zeroes, start, step, smallest):
+def crack_worker(secret_key, leading_zeroes, start, step, smallest: multiprocessing.Value):
     """Terminates once finding a working hash, or if it passes the current winner
     Checks current winner every CHECK_FREQ iterations to reduce lock delays"""
     target = '0' * leading_zeroes
     for i, addendum in enumerate(itertools.count(start, step)):
         if compute_hash(secret_key, addendum).startswith(target):
-            if smallest.value == 0 or addendum < smallest.value:
-                smallest.value = addendum
+            smallest.value = min(smallest.value, addendum)
             return
-        if i % CHECK_FREQ == 0 and smallest.value != 0 and addendum >= smallest.value:
+        if i % CHECK_FREQ == 0 and addendum >= smallest.value:
             return
 
 
 def crack_hash(secret_key, leading_zeroes, start=0) -> int:
-    smallest = multiprocessing.Value('i', 0)
+    smallest = multiprocessing.Value('f', math.inf)
     num_cores = multiprocessing.cpu_count()
     threads = [multiprocessing.Process(target=crack_worker,
                                        args=(secret_key, leading_zeroes, start + increment, num_cores, smallest))
@@ -38,7 +38,7 @@ def crack_hash(secret_key, leading_zeroes, start=0) -> int:
     for thread in threads:  # Wait for all threads to finish
         thread.join()
 
-    return smallest.value
+    return int(smallest.value)
 
 
 def main():
