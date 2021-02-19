374e2f178a538e43ee82960ca034570438c5871e
Andrew Kirk <ajskirk@gmail.com>
2021-02-19 17:00:04 +0000

    Include syndrome reporting for better testability

--- python/y_2015/day05.py
+++ python/y_2015/day05.py
@@ -1,7 +1,9 @@
 import re
 import sys
-from typing import Tuple
+from typing import Tuple, Iterable
 
+
+Syndrome = Iterable[bool]
 
 TEST_SETS = ((r"(.*[aeiou]){3}",        # Three vowels with anything in between
               r".*(.)\1",               # Repeated letter
@@ -11,15 +13,15 @@ TEST_SETS = ((r"(.*[aeiou]){3}",        # Three vowels with anything in between
               r".*(.).\1"))             # Repeated letter with single char in middle
 
 
-def is_nice(candidate: str, tests: Tuple[str, ...]) -> bool:
-    return all(re.match(test, candidate) is not None for test in tests)
+def is_nice(candidate: str, tests: Tuple[str, ...]) -> Syndrome:
+    return (re.match(test, candidate) is not None for test in tests)
 
 
 def main():
     with open(sys.argv[1], "r") as f:
         for test_set in TEST_SETS:
             f.seek(0)
-            print("Nice strings: {}".format(sum(1 for cand in f if is_nice(cand, test_set))))
+            print("Nice strings: {}".format(sum(all(is_nice(cand, test_set)) for cand in f)))
 
 
 if __name__ == "__main__":
