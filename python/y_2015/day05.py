b384213cf3472fb39cb71ed616182bce8b3119f0
Andrew Kirk <ajskirk@gmail.com>
2021-02-19 12:20:42 +0000

    Day 5 complete

--- python/y_2015/day05.py
+++ python/y_2015/day05.py
@@ -1,17 +1,25 @@
 import re
 import sys
+from typing import Tuple
 
 
-def is_nice(candidate: str) -> bool:
-    tests = (r"(.*[aeiou]){3}",  # Three vowels with anything in between
-             r".*(.)\1",  # Repeated letter
-             r"(?!.*(ab|cd|pq|xy))")  # No naughty pairs anywhere
+TEST_SETS = ((r"(.*[aeiou]){3}",  # Three vowels with anything in between
+              r".*(.)\1",  # Repeated letter
+              r"(?!.*(ab|cd|pq|xy))"),  # No naughty pairs anywhere
+
+             (r".*(..).*\1",
+              r".*(.).\1"))
+
+
+def is_nice(candidate: str, tests: Tuple[str, ...]) -> bool:
     return all(re.match(test, candidate) is not None for test in tests)
 
 
 def main():
     with open(sys.argv[1], "r") as f:
-        print("Nice strings: {}".format(sum(1 for cand in f if is_nice(cand))))
+        for test_set in TEST_SETS:
+            f.seek(0)
+            print("Nice strings: {}".format(sum(1 for cand in f if is_nice(cand, test_set))))
 
 
 if __name__ == "__main__":
