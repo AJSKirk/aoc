2779833323a58be8aedaceaa5846e46df1b2f544
Andrew Kirk <ajskirk@gmail.com>
2021-02-25 14:56:52 +0000

    Convert to numpy - Runs 8x faster

--- python/y_2015/day06.py
+++ python/y_2015/day06.py
@@ -2,16 +2,16 @@ from typing import List
 import re
 import sys
 from collections import namedtuple
-from itertools import product
-
+import numpy as np
 
 Vector = namedtuple('Vector', ['x', 'y'])
 
 
 N_DIM = 1000
+DIMS = 2
 OPERATIONS = {'turn on': lambda x: 1,
               'turn off': lambda x: 0,
-              'toggle': lambda x: int(not x)}
+              'toggle': lambda x: x * -1 + 1}
 
 
 def parse(line: str) -> (str, List[Vector]):
@@ -22,12 +22,13 @@ def parse(line: str) -> (str, List[Vector]):
 
 
 def main():
-    grid = [[0 for _ in range(N_DIM)] for _ in range(N_DIM)]
+    grid = np.zeros(tuple(N_DIM for _ in range(DIMS)), np.byte)
     with open(sys.argv[1], 'r') as f:
         for line in f:
             op, corners = parse(line)
-            for row, col in product(range(corners[0].x, corners[1].x + 1), range(corners[0].y, corners[1].y + 1)):
-                grid[row][col] = op(grid[row][col])
+            # Next two lines are ugly AF, but necessary due to NumPy's python-breaking indexing DSL
+            target = grid[corners[0].x:corners[1].x+1, corners[0].y:corners[1].y+1]
+            grid[corners[0].x:corners[1].x+1, corners[0].y:corners[1].y+1] = op(target)
 
     print(sum(sum(col for col in row) for row in grid))
 
