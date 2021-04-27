d25e3b2cce72060c1d847cc665c96d473ba24802
Andrew Kirk <ajskirk@gmail.com>
2021-04-27 15:03:55 +0100

    Improve Part 1 solution

--- fortran/2019/day01.f90
+++ fortran/2019/day01.f90
@@ -1,35 +1,46 @@
 program day01
     implicit none
 
-    character(len=32) :: filename
-    integer :: ios
+
+    integer, parameter :: max_modules = 256
+    integer, parameter :: max_iters = 64
+
     integer :: nvalues
     integer :: i
-    integer, dimension(256) :: mass
-    integer, dimension(64,256) :: fuels
+    integer, dimension(max_modules) :: mass
+    integer, dimension(max_modules,max_iters) :: fuels
 
-    ! Read in file
-    call getarg(1, filename)
-    open(unit=1, file=filename)
+    call read_stdin(mass, nvalues, max_modules)
+
+    ! Solve Part 1
+    fuels(:,1) = mass / 3 - 2
+
+    ! Solve Part 2
+    do i=2,max_iters
+        fuels(:,i) = max(fuels(:,i-1) / 3 - 2, 0)
+        if (sum(fuels(:,i)) == 0) exit
+    end do
+
+    print *, 'Naive fuel required: ', sum(fuels(1:nvalues,1))
+    print *, 'Total fuel required: ', sum(fuels(1:nvalues,:i))
+
+end program day01
+
+subroutine read_stdin(mass, nvalues, max_modules)
+    use, intrinsic :: iso_fortran_env, only : input_unit
+    implicit none
+
+    integer, intent(in) :: max_modules
+    integer, intent(out) :: mass(max_modules)
+    integer, intent(out) :: nvalues
+
+    integer :: ios
+
     nvalues = 1
     do
-        read(1, *, iostat=ios) mass(nvalues)
-        if (ios /= 0) then
-            exit
-        end if
+        read(input_unit, *, iostat=ios) mass(nvalues)
+        if (ios /= 0) exit
         nvalues = nvalues + 1
     end do
     nvalues = nvalues - 1
-    close(1)
-
-    fuels(1,:) = mass / 3 - 2
-
-    do i=2,64
-        fuels(i,:) = max(fuels(i-1,:) / 3 - 2, 0)
-        if (sum(fuels(i,:)) == 0) exit
-    end do
-
-    print *, 'Naive fuel required: ', sum(fuels(1,1:nvalues))
-    print *, 'Total fuel required: ', sum(fuels(1:i,1:nvalues))
-
-end program day01
+end subroutine read_stdin
