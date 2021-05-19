ca5abcae6d4636fef0d47c5f728747dab0d9ba4a
Andrew Kirk <ajskirk@gmail.com>
2021-05-19 10:27:16 +0100

    Finished Day 14

--- python/y_2015/day14.py
+++ python/y_2015/day14.py
@@ -1,6 +1,7 @@
 import sys
 import re
 from collections import namedtuple
+from typing import Dict, Generator
 
 RACE_TIME = 2503
 
@@ -14,12 +15,27 @@ def parse(line: str):
 
 
 def make_generator(reindeer: Reindeer):
-    """A Reindeer class might be more intuitive, but making generators to hold state seemed like more fun"""
+    """A Reindeer class might be more intuitive, but making generators to hold state seemed like more fun.
+    Using stepwise simulations rather than an analytic answer."""
+    dist = 0
     while True:
         for _ in range(reindeer.move_time):
-            yield reindeer.speed
+            yield (dist := dist + reindeer.speed)
         for _ in range(reindeer.rest_time):
-            yield 0
+            yield dist
+
+
+def run_race(reindeers: Dict[str, Generator]) -> (Dict[str, int], Dict[str, float]):
+    """Return points, distances"""
+    points = {name: 0 for name in reindeers}
+    for _ in range(RACE_TIME):
+        distances = {name: next(gen) for name, gen in reindeers.items()}
+        leaders = (name for name in distances if distances[name] == max(distances.values()))
+        for leader in leaders:
+            points[leader] += 1
+
+    return points, distances
+
 
 
 def main():
@@ -27,10 +43,12 @@ def main():
     with open(sys.argv[1], 'r') as f:
         reindeers.update(parse(line) for line in f)
 
-    distances = {name: sum(step for _, step in zip(range(RACE_TIME), reindeers[name])) for name in reindeers}
+    points, distances = run_race(reindeers)
     winner = max(distances, key=distances.get)
-
     print("The winner is {}, having travelled {} km".format(winner, distances[winner]))
+
+    winner = max(points, key=points.get)
+    print("The new winner is {}, having accumulated {} points".format(winner, points[winner]))
 
 
 if __name__ == '__main__':
