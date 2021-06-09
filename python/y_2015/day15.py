3a4aad3f1b8e5d59c4c66825155f4ed0133ae729
Andrew Kirk <ajskirk@gmail.com>
2021-06-10 00:28:32 +0100

    Lagrangian formulation working

--- python/y_2015/day15.py
+++ python/y_2015/day15.py
@@ -2,7 +2,7 @@ import re
 import sys
 from typing import Tuple, Dict
 import itertools
-import sympy
+import sympy as sp
 
 
 BUDGET = 100
@@ -16,39 +16,33 @@ def parse(line: str) -> Tuple[str, IngredientValues]:
     return name, {k: int(v) for k, v in re.findall(pattern, raw_components)}
 
 
-def score_recipe(ingredients: Dict[str, IngredientValues], allocations: Dict[str, int]):
-    assert sum(allocations.values()) == BUDGET
+def lagrange_multiplier(ingredients):
+    """Sympy is kinda slow so this isn't actually a particulatly fast solution, but it's just cool to show that there's
+    an (almost) analytic solution available."""
+    xs = sp.symbols('x:{}'.format(len(ingredients)))
+    lmb = sp.symbols('lmb')
 
-    score = 1
+    # SKip negative logic due to differentiability requirements
+    objective = 1
     for attribute in filter(lambda x: x != "calories", next(iter(ingredients.values())).keys()):
-        score *= sum(ingredients[name][attribute] * allocations[name] for name in allocations.keys())
-        if score <= 0:
-            return 0
+        objective *= sum(list(ingredients.values())[i][attribute] * x for i, x in enumerate(xs))
 
-    return score
+    constraint = sum(xs) - BUDGET
 
+    L = objective - lmb * constraint
+    grad_L = [sp.diff(L, wrt) for wrt in (*xs, lmb)]  # Vector calc time
+    candidates = sp.solve(grad_L, (*xs, lmb), dict=True)  # Stationary points of L
 
-def brute_force_max(ingredients):
-    """Just brute force it and rely on the score early stopping to help speed up. Will refactor to a Lagrangian multiplier
-    when I have time"""
-
-    alloc_vectors = filter(lambda x: sum(x) == 100, itertools.product(range(BUDGET + 1), repeat=len(ingredients)))
-    return max(score_recipe(ingredients, dict(zip(ingredients.keys(), alloc))) for alloc in alloc_vectors)
-
-
-def compile_lagrange_multiplier(ingredients):
-    xs = sympy.symbols('x:{}'.format(len(ingredients)))
-
-    # Going to skip the negative logic for the moment
-    objective = ingredients
-
+    # Find the optimal stationary point
+    optimal_float = max(candidates, key=lambda assignment: objective.subs(assignment))
+    return optimal_float
 
 
 def main():
     with open(sys.argv[1], 'r') as f:
         ingredients = dict(parse(line) for line in f)
 
-    print(brute_force_max(ingredients))
+    print(lagrange_multiplier(ingredients))
 
 
 if __name__ == "__main__":
