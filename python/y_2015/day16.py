d361717c807b3d6706a04a45fbf6c8dfd54cf92a
Andrew Kirk <ajskirk@gmail.com>
2021-06-21 17:17:14 +0100

    Day 16 part 2

--- python/y_2015/day16.py
+++ python/y_2015/day16.py
@@ -1,22 +1,25 @@
 import sys
 import re
 from typing import Dict, Tuple
+from operator import eq, gt, lt
+from collections import namedtuple
 
 
 Sue = Dict[str, int]
+ReportClause = namedtuple('ReportClause', ('op', 'val'))
 
 
 MFCSAM_REPORT = {
-    'children': 3,
-    'cats': 7,
-    'samoyeds': 2,
-    'pomeranians': 3,
-    'akitas': 0,
-    'vizslas': 0,
-    'goldfish': 5,
-    'trees': 3,
-    'cars': 2,
-    'perfumes': 1
+    'children': ReportClause(eq, 3),
+    'cats': ReportClause(gt, 7),
+    'samoyeds': ReportClause(eq, 2),
+    'pomeranians': ReportClause(lt, 3),
+    'akitas': ReportClause(eq, 0),
+    'vizslas': ReportClause(eq, 0),
+    'goldfish': ReportClause(lt, 5),
+    'trees': ReportClause(gt, 3),
+    'cars': ReportClause(eq, 2),
+    'perfumes': ReportClause(eq, 1)
 }
 
 
@@ -26,15 +29,16 @@ def parse(line: str) -> Tuple[int, Sue]:
     return suenum, {k: int(v) for k, v in re.findall(pattern, line)}
 
 
-def validate_sue(sue: Sue, report=MFCSAM_REPORT) -> bool:
-    return all(sue.get(key, rep_value) == rep_value for key, rep_value in report.items())
+def validate_sue(sue: Sue, old_retroencabulator_mode: bool, report=MFCSAM_REPORT) -> bool:
+    return all((report[key].op if old_retroencabulator_mode else eq)(sue[key], report[key].val) for key in sue)
 
 
 def main():
     with open(sys.argv[1], 'r') as f:
         sues = dict(parse(line) for line in f)
 
-    print('The Correct Sue ID is: {}'.format(next(suenum for suenum, sue in sues.items() if validate_sue(sue))))
+    print('The Correct Sue ID is: {}'.format(next(suenum for suenum, sue in sues.items() if validate_sue(sue, False))))
+    print('The Real Correct Sue ID is: {}'.format(next(suenum for suenum, sue in sues.items() if validate_sue(sue, True))))
 
 
 if __name__ == '__main__':
