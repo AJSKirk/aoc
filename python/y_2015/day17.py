6e47925ee4d9775713c54766a5edb369a6f1cb61
Andrew Kirk <ajskirk@gmail.com>
2021-06-27 14:31:00 +0100

    Clean formatting on Day 17

--- python/y_2015/day17.py
+++ python/y_2015/day17.py
@@ -7,16 +7,16 @@ TOTAL_VOL = 150
 
 def possible_combos(to_store: int, bins: Iterable[int]):
     """Recursive - Relies on the fact that no bins have negative volume"""
-    if to_store < 0 or to_store > sum(bins):  # Early-pruning
-        return 0
     if to_store == 0:
         return 1
+    if to_store < 0 or to_store > sum(bins):  # Early-pruning
+        return 0
     return possible_combos(to_store, bins[1:]) + possible_combos(to_store - bins[0], bins[1:])
 
 
 def main():
     with open(sys.argv[1], 'r') as f:
-        bins = sorted([int(line.strip()) for line in f], reverse=True)
+        bins = sorted([int(line.strip()) for line in f], reverse=True)  # Sorting largest-first maximises pruning
 
     print("Possible Combinations: {}".format(possible_combos(TOTAL_VOL, bins)))
 
