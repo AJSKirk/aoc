3baff5ab0d945af9aa9258100ac0433e3c7c8496
Andrew Kirk <ajskirk@gmail.com>
2021-12-17 12:43:03 +0000

    Maybe I can do this whole think without a for loop

--- python/y_2021/day04.py
+++ python/y_2021/day04.py
@@ -1,5 +1,6 @@
 import sys
 from itertools import takewhile, accumulate
+from functools import reduce
 
 
 class Board:
@@ -13,13 +14,18 @@ class Board:
     def is_winner(self, seen_numbers):
         if any(map(lambda board: any(all(cell in seen_numbers for cell in row) for row in board),
                        (self.board, self.board_transposed))):
-            return self.score(seen_numbers)
+            score = self.score(seen_numbers)
+            self.wipe()
+            return score
         else:
             return 0
 
     def score(self, seen_numbers):
         unmarked = sum(sum(cell for cell in row if cell not in seen_numbers) for row in self.board)
         return unmarked * seen_numbers[-1]
+
+    def wipe(self):
+        self.board = [[0 for _ in self.board[0]] for row in self.board]
 
 
 def chunkerator(instructions):
@@ -48,6 +54,7 @@ def main():
     numbers, boards = parse(sys.stdin)
 
     print(f"Score of the first Complete Board: {next(s for s in play_bingo(numbers, boards) if s != 0)}")
+    print(f"Score of the last Complete Board: {reduce(lambda s, n: n if n != 0 else s, play_bingo(numbers, boards))}")
 
 
 if __name__ == "__main__":
