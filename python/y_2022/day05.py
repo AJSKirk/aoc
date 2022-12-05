33d36df49104f50bfd532615ee2f093b492c5f88
Andrew Kirk <ajskirk@gmail.com>
2022-12-05 13:51:53 +0000

    Day 5 done

--- python/y_2022/day05.py
+++ python/y_2022/day05.py
@@ -2,6 +2,7 @@ import fileinput
 from collections import namedtuple
 import re
 from typing import List
+from copy import deepcopy
 
 
 Instruction = namedtuple('Instruction', ['amt', 'fr', 'to'])
@@ -33,19 +34,28 @@ def build_stacks(input) -> List[Stack]:
     return stacks
 
 
-def execute_instruction(stacks: List[Stack], inst: Instruction) -> None:  # Mutates stacks, no return
-    for _ in range(inst.amt):
-        stacks[inst.to - 1].append(stacks[inst.fr - 1].pop())
+def execute_instruction(stacks: List[Stack], inst: Instruction, cranemodel=9000) -> None:  # Mutates stacks, no return
+    assert cranemodel in (9000, 9001)
+    if cranemodel == 9000:
+        for _ in range(inst.amt):
+            stacks[inst.to - 1].append(stacks[inst.fr - 1].pop())
+    else:
+        stacks[inst.to - 1].extend(stacks[inst.fr - 1][-inst.amt:])  # Extend, not append!
+        del stacks[inst.fr - 1][-inst.amt:]
 
 
 def main():
     with fileinput.input() as input:
         stacks = build_stacks(input)
+        multistacks = [deepcopy(stacks) for _ in range(2)]  # Copy and simulate in parallel
         input.readline()  # Clear blank line
         for line in input:
-            execute_instruction(stacks, parse_instruction(line.strip()))
+            instruction = parse_instruction(line.strip())
+            execute_instruction(multistacks[0], instruction)
+            execute_instruction(multistacks[1], instruction, cranemodel=9001)
 
-    print(''.join(s[-1] for s in stacks))  # Print top crates
+    for idx, stacks in enumerate(multistacks):
+        print(f"Simulation {idx + 1}: {''.join(s[-1] for s in stacks)}")  # Print top crates
 
 
 if __name__ == "__main__":
